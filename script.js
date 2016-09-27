8e41f8f11bf643b79e0c48f24c661ebb53aee78d
Charlene Becker <ourschar@gmail.com>
2016-09-27 12:53:20 -0400

    Created average age function

---
+++ script.js
@@ -0,0 +1,16 @@
+function averageAge() {
+	var cori = 38;
+	var charlene = 38;
+	var sienna = 7;
+	var jayde = 5;	
+	var familyAge = cori + charlene + sienna + jayde;
+	familyAge = familyAge / 4;
+
+	alert("the average age of the family is"+ " " + familyAge);
+	return;
+
+}
+
+averageAge();
+
+
